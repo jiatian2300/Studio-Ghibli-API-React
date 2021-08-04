cdea068aacbaabb86cc9e8477f147d7df5a8243d
jiatian2300 <jiatian2300@gmail.com>
2021-08-04 12:12:53 +0800

    able to generate questions for people category and check for correct answers

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -4,16 +4,23 @@ import noface from "../images/noface.png";
 
 function Quiz({ match, quizCat, setQuizCat }) {
     const [questions, setQuestions] = useState([]);
+    const [score, setScore] = useState(0);
+    const [qnNo, setQnNo] = useState(1);
+    const [currentQn, setCurrentQn] = useState({});
 
     // const [start, setStart] = useState(false);
-    const [start, setStart] = useState(true);
+    const [startPage, setStart] = useState(true);
 
     useEffect(() => {
         setQuizCat(match.params.category);
     }, [match, setQuizCat]);
 
     useEffect(() => {
-        processQns();
+        getNextQn();
+    }, [questions]);
+
+    useEffect(() => {
+        generateQns();
     }, []);
 
     const fetchItems = async (target) => {
@@ -22,19 +29,34 @@ function Quiz({ match, quizCat, setQuizCat }) {
         return items;
     };
 
-    const processQns = async () => {
+    const generateQns = async () => {
         const films = await fetchItems(`https://ghibliapi.herokuapp.com/films`);
-
         const items = await fetchItems(
             `https://ghibliapi.herokuapp.com/${quizCat}`
         );
 
         items.map((item) => {
-            const question = `${item.name} is a character from the movie, ${
-                films[getRandomInt(films.length)].title
-            }`;
+            const film_url = item.films[0].split("/");
+            const film_ans = films.filter(
+                (x) => x.id === film_url[film_url.length - 1]
+            );
+            //ensure that there is always a 50% chance of each of the options being correct
+            const film_option =
+                Math.random() < 0.5
+                    ? films[getRandomInt(films.length)]
+                    : film_ans[0];
 
-            setQuestions((questions) => [...questions, question]); // MUST PASS questions AS A PARAMETER??
+            const question = `${item.name} is a character from the movie, ${film_option.title}`;
+
+            setQuestions((questions) => [
+                ...questions,
+                {
+                    question: question,
+                    option: film_option.id,
+                    answer: film_ans[0].id,
+                },
+            ]); // MUST PASS questions AS A PARAMETER??
+            return true;
         });
     };
 
@@ -42,8 +64,27 @@ function Quiz({ match, quizCat, setQuizCat }) {
         return Math.floor(Math.random() * max);
     }
 
+    function getNextQn() {
+        if (questions.length > 0) {
+            setCurrentQn(questions[getRandomInt(questions.length)]);
+        }
+    }
+
+    function checkAns(choice) {
+        const correct = currentQn.option === currentQn.answer ? true : false;
+        if (choice === correct) {
+            setScore(score + 1);
+        }
+        setQnNo(qnNo + 1);
+        getNextQn();
+    }
+
     return (
-        <div className={`container ${start ? "quiz_screen" : "start_screen"}`}>
+        <div
+            className={`container ${
+                startPage ? "quiz_screen" : "start_screen"
+            }`}
+        >
             <div className="start">
                 <h1>{quizCat}</h1>
                 <button
@@ -65,17 +106,17 @@ function Quiz({ match, quizCat, setQuizCat }) {
                 </header>
                 <div className="stats">
                     <div className="stat_txt">
-                        Q<u>n</u>1<span>/10</span>
+                        Q<u>n</u>
+                        {qnNo}
+                        <span>/10</span>
                     </div>
                     <div className="score">
                         <div className="stat_txt score_txt">Score</div>
-                        <div className="score_value">8</div>
+                        <div className="score_value">{score}</div>
                     </div>
                 </div>
                 <div className="question">
-                    <div className="question_txt">
-                        {questions[getRandomInt(questions.length)]}
-                    </div>
+                    <div className="question_txt">{currentQn.question}</div>
                     <img
                         src={noface}
                         alt="No Face Character"
@@ -83,8 +124,18 @@ function Quiz({ match, quizCat, setQuizCat }) {
                     />
                 </div>
                 <div className="options">
-                    <button className="true option_btn">TRUE</button>
-                    <button className="false option_btn">FALSE</button>
+                    <button
+                        className="true option_btn"
+                        onClick={() => checkAns(true)}
+                    >
+                        TRUE
+                    </button>
+                    <button
+                        className="false option_btn"
+                        onClick={() => checkAns(false)}
+                    >
+                        FALSE
+                    </button>
                 </div>
             </div>
         </div>
